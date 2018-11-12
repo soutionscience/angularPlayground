822b9f0797ed0f22f4976dbd26c060a1cc8b1973
soutionscience <marshnjagi@yahoo.com>
2018-11-12 20:57:44 +0300

    started animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ElementRef, HostListener } from '@angular/core';
+import { flyInOut } from '../animations/app-animations';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations:[
+    flyInOut()
+  ]
 })
 export class AboutComponent implements OnInit {
+   state='hide'
 
   constructor() { }
 
+  // @HostListener('window:scroll', ['$event'])
+  // checkScroll(){
+  //   console.log("check scroll")
+ 
+  //   const componentPosition = this.el.nativeElement.offsetTop
+  //   const scrollPosition = document.documentElement.scrollTop + 430;
+  //   console.log("scroll posion is ", document.documentElement.scrollTop)
+  //   console.log("component postion is ", this.el.nativeElement.offsetTop)
+
+  //   if(scrollPosition >= componentPosition){
+  //     this.state = 'show'
+  //   }else{
+  //     this.state = 'hide'
+  //   }
+  // }
+
+
   ngOnInit() {
+    this.state='show'
   }
 
 }
--- src/app/animations/app-animations.ts
+++ src/app/animations/app-animations.ts
@@ -1,1 +1,17 @@
-import {trigger, state, transition} from '@angular/animations'
+import {trigger, state, transition, style, animate, stagger} from '@angular/animations'
+
+
+export function flyInOut(){
+     return trigger('flyInOut', [
+         state('show', style({
+            opacity: 1,
+            transform: "translateX(0)"
+         })),
+         state('hide', style({
+             opacity: 0,
+             transform: "translateX(-100%)"
+         })),
+         transition('show => hide', animate('750ms {delay: 500ms} ease-out')),
+         transition('* => show', animate('750ms  ease-in'))
+     ])
+}
